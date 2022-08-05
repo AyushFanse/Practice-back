1392a8df1146ff0b2f515adcb0ec2e654f04ec08
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2022-08-05 23:40:53 +0530

    Task 1

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../modules/userModule");
+const User = require("../modules/UserModule");
 
 //~------------------------| GET USER DATA |------------------------~//
 
